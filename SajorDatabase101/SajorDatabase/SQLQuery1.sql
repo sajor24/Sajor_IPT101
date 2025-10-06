38486592367dc23f015fa637d79a536ff3d21243
sajor24 <143572576+sajor24@users.noreply.github.com>
2025-10-06 20:51:59 +0800

    Delete SajorDatabase101 directory

--- SajorDatabase101/SajorDatabase/SQLQuery1.sql
+++
@@ -1,3 +0,0 @@
-﻿exec Read_Employee
-Insert Into Employee (FirstName,LastName,Age,Position)
-Values ('Irving','Sajor','20','Developer')
--- SajorDatabase101/SajorDatabase/Stored Procedure/Delete_Employee.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[DeleteEmployee]
-    @Emp_ID INT
-AS
-BEGIN
-    DELETE FROM [dbo].[Employee]
-    WHERE [Emp_ID] = @Emp_ID;
-END
