23e822de29a4c6de6da9b592c6ddaffa06f2509d
sajor24 <143572576+sajor24@users.noreply.github.com>
2025-11-03 23:21:22 +0800

    Add files via upload

---
+++ SajorIPT101/SajorDatabase/Stored Procedure/Create_Employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE Create_Employee
+    @FirstName NVARCHAR(50) = NULL,
+    @LastName  NVARCHAR(50) = NULL,
+    @Age NVARCHAR(50) = NULL,
+    @Position NVARCHAR(50) = NULL
+AS
+BEGIN
+    INSERT INTO [dbo].[Employee] ([FirstName], [LastName], [Age], [Position])
+    VALUES (@FirstName, @LastName, @Age, @Position);
+END
