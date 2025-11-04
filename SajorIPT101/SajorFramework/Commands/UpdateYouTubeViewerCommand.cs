8eb6c0f60483e5af863906e16dd57e807b3b271b
sajor24 <143572576+sajor24@users.noreply.github.com>
2025-11-04 17:50:21 +0800

    Add files via upload

---
+++ SajorIPT101/SajorFramework/Commands/UpdateYouTubeViewerCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SajorIPT101Solution.SajorDomain.Commands;
+using SajorIPT101Solution.SajorDomain.Models;
+using SajorIPT101Solution.SajorFramework.DTOs;
+
+namespace SajorIPT101Solution.SajorFramework.Commands
+{
+    public class UpdateYouTubeViewerCommand : IUpdateYouTubeViewerCommand
+    {
+        private readonly YouTubeViewersDbContextFactory _contextFactory;
+
+        public UpdateYouTubeViewerCommand(YouTubeViewersDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task Execute(YouTubeViewer youTubeViewer)
+        {
+            using (YouTubeViewersDbContext context = _contextFactory.Create())
+            {
+                YouTubeViewerDto youTubeViewerDto = new YouTubeViewerDto()
+                {
+                    Id = youTubeViewer.Id,
+                    Username = youTubeViewer.Username,
+                    IsSubscribed = youTubeViewer.IsSubscribed,
+                    IsMember = youTubeViewer.IsMember,
+                };
+
+                context.YouTubeViewers.Update(youTubeViewerDto);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ SajorIPT101/SajorWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SajorWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
