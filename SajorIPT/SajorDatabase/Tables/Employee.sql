3b689e561fbaa31831c202d05a9c1b5b00ff326d
sajor24 <143572576+sajor24@users.noreply.github.com>
2025-11-04 18:24:55 +0800

    Add files via upload

---
+++ SajorIPT/SajorDatabase/Tables/Employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+
+    Emp_ID INT IDENTITY(1,1) PRIMARY KEY,
+    FirstName NVARCHAR(50) NULL,
+    LastName NVARCHAR(50) NULL,
+    Age INT NULL,
+    Position NVARCHAR(50) NULL
+
+)
---
+++ SajorIPT/SajorDomain/Commands/IUpdateYouTubeViewerCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SajorIPT101Solution.SajorDomain.Models;
+
+namespace SajorIPT101Solution.SajorDomain.Commands
+{
+    public interface IUpdateYouTubeViewerCommand
+    {
+        Task Execute(YouTubeViewer youTubeViewer);
+    }
+}
