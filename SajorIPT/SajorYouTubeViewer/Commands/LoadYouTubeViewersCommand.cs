a4393f2f9089cf359b649359012ca5792fabd085
sajor24 <143572576+sajor24@users.noreply.github.com>
2025-11-04 18:26:51 +0800

    Add files via upload

---
+++ SajorIPT/SajorYouTubeViewer/Commands/LoadYouTubeViewersCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SajorIPT101Solution.SajorYouTubeViewer.Stores;
+using SajorIPT101Solution.SajorYouTubeViewer.ViewModels;
+
+namespace SajorIPT101Solution.SajorYouTubeViewer.Commands
+{
+    public class LoadYouTubeViewersCommand : AsyncCommandBase
+    {
+        private readonly YouTubeViewersViewModel _youTubeViewersViewModel;
+        private readonly YouTubeViewersStore _youTubeViewersStore;
+
+        public LoadYouTubeViewersCommand(YouTubeViewersViewModel youTubeViewersViewModel, YouTubeViewersStore youTubeViewersStore)
+        {
+            _youTubeViewersViewModel = youTubeViewersViewModel;
+            _youTubeViewersStore = youTubeViewersStore;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            _youTubeViewersViewModel.ErrorMessage = null;
+            _youTubeViewersViewModel.IsLoading = true;
+
+            try
+            {
+                await _youTubeViewersStore.Load();
+            }
+            catch (Exception)
+            {
+                _youTubeViewersViewModel.ErrorMessage = "Failed to load YouTube viewers. Please restart the application.";
+            }
+            finally
+            {
+                _youTubeViewersViewModel.IsLoading = false;
+            }
+        }
+    }
+}
---
+++ SajorIPT/SajorYouTubeViewer/ViewModels/AddYouTubeViewerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using SajorIPT101Solution.SajorYouTubeViewer.Commands;
+using SajorIPT101Solution.SajorYouTubeViewer.Stores;
+
+namespace SajorIPT101Solution.SajorYouTubeViewer.ViewModels
+{
+    public class AddYouTubeViewerViewModel : ViewModelBase
+    {
+        public YouTubeViewerDetailsFormViewModel YouTubeViewerDetailsFormViewModel { get; }
+
+        public AddYouTubeViewerViewModel(YouTubeViewersStore youTubeViewersStore, ModalNavigationStore modalNavigationStore)
+        {
+            ICommand submitCommand = new AddYouTubeViewerCommand(this, youTubeViewersStore, modalNavigationStore);
+            ICommand cancelCommand = new CloseModalCommand(modalNavigationStore);
+            YouTubeViewerDetailsFormViewModel = new YouTubeViewerDetailsFormViewModel(submitCommand, cancelCommand);
+        }
+    }
+}
